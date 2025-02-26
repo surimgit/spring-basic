88d01a790fdfeb93e55d7655aea4a37b072706ec
surimgit <ruriaim@gmail.com>
2025-02-26 17:42:35 +0900

    2025-02-26

--- src/main/java/com/korit/basic/BasicApplication.java
+++ src/main/java/com/korit/basic/BasicApplication.java
@@ -3,6 +3,8 @@ package com.korit.basic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.korit.basic.dto.Lombok;
+
 @SpringBootApplication
 public class BasicApplication {
 
@@ -10,4 +12,14 @@ public class BasicApplication {
 		SpringApplication.run(BasicApplication.class, args);
 	}
 	
+	void method(){
+		Lombok lombok1 = new Lombok("a", 1, false, true);
+		Lombok lombok2 = new Lombok("a", 1);
+
+		lombok1.getField1();
+		lombok1.isField3();
+		lombok1.getField4();
+		lombok2.setField3(false);
+		lombok2.setField4(null);
+	}
 }
--- src/main/java/com/korit/basic/controller/RequestDataController.java
+++ src/main/java/com/korit/basic/controller/RequestDataController.java
@@ -1,9 +1,20 @@
 package com.korit.basic.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.korit.basic.dto.Validation;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @RestController
@@ -26,4 +37,54 @@ public class RequestDataController {
     return "이름: " + name + "나이: " + age;
   }
 
+  // @PathVariable(): requet의 url패턴에 따라 데이터를 추출하는 방법
+  // GET http://127.0.0.1:8080/request-data/path-variable
+  // 만약 PathVariable로 받아오는 매개변수가 선택적이라면 Path를 여러개 지정해야함
+  @GetMapping({"/path-variable/{name}/{ho}", "/path-variable/{name}", "/path-variable/{name}/"})
+  public String pathVariable(
+    @PathVariable("name") String name,
+    @PathVariable(name="ho", required=false) String ho
+  ){
+    return "이름: " + name + ",묘호: " + ho;
+  }
+
+  // path variable을 이용한 방법은 다른 메서드의 url패턴과 겹칠수 있음을 주의
+  @GetMapping("/{variable}")
+  public String sample(
+    @PathVariable("variable") String variable
+  ){
+    return variable;
+  }
+
+  // @RequestBody(): POST, PUT, PATCH, RequestBody로 전송한 데이터를 
+  // 메소드에서 매개변수로 받기 위한 방법
+
+  @PostMapping("/request-body")
+  public String requestBody(
+    @RequestBody() RequestBodyDto requestBody
+  ){
+    return "이름: " + requestBody.getName() + ", 나이: " + requestBody.getAge();
+  }
+
+  @PostMapping("/validation")
+  public String validation(
+    @RequestBody()@Valid Validation requestBody
+  ){
+    return requestBody.toString();
+  }
+
+}
+
+
+// DTO (Data Transfer Object)
+// - 데이터를 서로 다른 계층간에 전송하기 위한 객체
+// - 캡슐화가 되어 있음, 비즈니스 로직은 포함하지 않음
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+class RequestBodyDto{
+  private String name;
+  private Integer age;
 }
